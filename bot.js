df5a3dcd83f5e72e2504dc68f91222007439f34a
Glitch (hello-express) <none>
2020-04-17 18:07:00 +0000

    🎥⭐️ Checkpoint ./server.js:8490860/483 ./watch.json:8490860/91 ./bot.js:8490860/3104 ./package.json:8490860/3970 ./.trigger-rebuild:8490860/105

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+
+const client = new Discord.Client();
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.content.startsWith("cat")) {
+    let embed = new Discord.MessageEmbed();
+    const { file } = await fetch("https://aws.random.cat/meow").then(response =>
+      response.json()
+    );
+    embed.setTitle("Your Cat");
+    embed.setImage(file);
+    embed.setColor("#" + Math.floor(Math.random() * 16777215).toString(16));
+    return message.channel.send(embed);
+  }
+});
+
+client.login(process.env.TOKEN);
