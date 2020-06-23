e12eeaa022a3f8360b013b343a4f02c091e00d80
Team Conqr <34463817+conqr2@users.noreply.github.com>
2020-06-22 18:03:10 -0700

    Update server.js
    docs

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+//For glitch apps, if you're using heroku or something you might need this too, otherwise change package.json "start" to "bot.js"
 const express = require("express");
 const app = express();
 
 require("./bot.js");
 // https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
+app.get("/*", (request, response) => {
   response.status(200);
   response.send("200");
 });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log("Listening on port " + listener.address().port);
 });
